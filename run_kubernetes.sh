6d0846911354bea1a7bba37ca5c153f5d5d55e64
EC2 Default User <ec2-user@ip-172-31-0-9.ec2.internal>
2022-08-27 18:57:00 +0000

    rund kubernetes & kubernetes out

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="rpa495/ml-microservice:v1.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlmicroservice --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlmicroservice 8000:80
